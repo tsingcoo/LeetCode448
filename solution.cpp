263d8b1e687abb714610a26318dbdd77b8557b79
wangqinglong <tsingcoo.wang@gmail.com>
2017-03-08 23:25:40 +0800

    Initial commit

---
+++ solution.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 王青龙 on 2017/3/8.
+//
+
+#include "solution.h"
+
+std::vector<int> Solution::findDisappearedNumbers(std::vector<int> &nums) {
+    return std::vector<int>();
+}
---
+++ solution.h
@@ -0,0 +1,15 @@
+//
+// Created by 王青龙 on 2017/3/8.
+//
+
+#ifndef LEETCODE448_SOLUTION_H
+#define LEETCODE448_SOLUTION_H
+
+#include <vector>
+
+class Solution{
+public:
+    std::vector<int> findDisappearedNumbers(std::vector<int>& nums);
+};
+
+#endif //LEETCODE448_SOLUTION_H
